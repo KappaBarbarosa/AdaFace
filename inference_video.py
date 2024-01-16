4e0aa8bf84d182e926df697ef0e28794c71d3fb5
Kappa7077 <94050924+KappaBarbarosa@users.noreply.github.com>
2024-01-16 20:38:43 +0800

    Update inference_video.py

--- inference_video.py
+++ inference_video.py
@@ -6,12 +6,13 @@ import matplotlib.pyplot as plt
 from yolo_face.align import YOLO_FACE
 import warnings
 import sys
+import argparse
 # ignore all the SyntaxWarning
 warnings.filterwarnings("ignore", category=SyntaxWarning)
 
 adaface_models = {
-    'ir_50':"pretrained/epoch=14-step=166804.ckpt",
-    # 'ir_50':"pretrained/adaface_ir50_ms1mv2.ckpt",
+    # 'ir_50':"pretrained/epoch=14-step=166804.ckpt",
+    'ir_50':"pretrained/adaface_ir50_masked_ms1mv2.ckpt",
     'ir_101':"pretrained/adaface_ir101_ms1mv2.ckpt",
 }
 
@@ -44,12 +45,18 @@ def save_database_heatmap(data):
     plt.savefig('iference_heatmap_101.png')
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--fd_weights', nargs='+', type=str, default='pretrained/yolov7-tiny.pt', help='face detection model.pt path(s)')
+    parser.add_argument('--database_path', type=str,default='face_database',help='face database path')
+    parser.add_argument('--database_dir', type=str,default='face_database',help='face database ckpt save path')
+    parser.add_argument('--source', type=str,default='wang_test_images',help='string for img dir, number for webcam')
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = load_pretrained_model('ir_50').to(device)
-    database_dir = './face_database/database.pt'
+    args = parser.parse_args()
+    database_dir = os.path.join(args.database_dir,'database.pt') 
     if(os.path.exists(database_dir)):
         database = torch.load(database_dir).to(device)
-        ID_list = sorted(os.listdir('face_database/ID'))
+        ID_list = sorted(os.listdir(os.path.join(args.database_dir,'ID')))
         print("ID_list's len: ", len(ID_list))
     else:
         print("ERROR: Face database not found. Please create a face database first.")
@@ -59,5 +66,5 @@ if __name__ == '__main__':
     features = []
     face=[]
     isyolo = True
-    yoloface = YOLO_FACE('yolo_face/yolov7-tiny.pt',device=device) if isyolo else None
-    yoloface.video_detect(0,fr_model= model,view_img= True,database=database,ID_list=ID_list)
+    yoloface = YOLO_FACE(argparse.fd_weights,device=device) if isyolo else None
+    yoloface.video_detect(args.source,fr_model= model,view_img= True,database=database,ID_list=ID_list)
